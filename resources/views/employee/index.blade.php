853775ccb3bec241fb3603199b43e43307e3f547
alighirbn <alighirbn@gmail.com>
2023-04-12 01:29:32 +0300

    Employee index success massage bug fixed

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -2,23 +2,22 @@
 
     <x-slot name="header">
 
-
         @include('employee.nav.navigation')
-
-        @if ($message = Session::get('success'))
-            <div class="alert alert-success">
-                <p>{{ $message }}</p>
-            </div>
-        @endif
-
 
     </x-slot>
 
-    <div class="py-6">
+    <div class="py-4">
         <div class="max-w-full mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {!! $dataTable->table() !!}
+                    @if ($message = Session::get('success'))
+                        <div class="alert alert-success">
+                            <p>{{ $message }}</p>
+                        </div>
+                    @endif
+                    <table>
+                        {!! $dataTable->table() !!}
+                    </table>
                 </div>
             </div>
         </div>
--- resources/views/employee/nav/navigation.blade.php
+++ resources/views/employee/nav/navigation.blade.php
@@ -1,9 +1,8 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white ">
     <!-- Primary Navigation Menu -->
     <div class=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between ">
             <div class="flex">
-
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
