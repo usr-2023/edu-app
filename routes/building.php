561a89b9a6c821b7795007fc39192bcab6d46124
alighirbn <alighirbn@gmail.com>
2023-04-19 20:48:14 +0300

    Building nav and routes

--- routes/building.php
+++ routes/building.php
@@ -1,14 +1,23 @@
-<?php
 
+
+<?php
 use App\Http\Controllers\BuildingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/building',[BuildingController::class,'index'])->middleware(['auth','verified'])->name('building.index');
-Route::get('/building/create',[BuildingController::class,'create'])->middleware(['auth','verified'])->name('building.create');
-Route::post('/building/create', [BuildingController::class, 'store'])->middleware(['auth','verified'])->name('building');
 
-Route::get('/building/show/{url_address}',[BuildingController::class,'show'])->middleware(['auth','verified'])->name('building.show');
+Route::group(['prefix' => 'building'], function() {
+    //index
+    Route::get('/',[BuildingController::class,'index'])->middleware(['auth','verified'])->name('building.index');
+    //create
+    Route::get('/create',[BuildingController::class,'create'])->middleware(['auth','verified'])->name('building.create');
+    Route::post('/create', [BuildingController::class, 'store'])->middleware(['auth','verified'])->name('building.store');
+    //show
+    Route::get('/show/{url_address}',[BuildingController::class,'show'])->middleware(['auth','verified'])->name('building.show');
+    //update
+    Route::get('/edit/{url_address}',[BuildingController::class,'edit'])->middleware(['auth','verified'])->name('building.edit');
+    Route::patch('/update/{url_address}', [BuildingController::class, 'update'])->middleware(['auth','verified'])->name('building.update');
+    //delete
+    Route::delete('/delete/{url_address}', [BuildingController::class, 'destroy'])->middleware(['auth','verified'])->name('building.destroy');
+    
+});
 
-Route::get('/building/edit/{url_address}',[BuildingController::class,'edit'])->middleware(['auth','verified'])->name('building.edit');
-Route::post('/building/update/{url_address}', [BuildingController::class, 'update'])->middleware(['auth','verified'])->name('building.update');
-Route::post('/building/delete/{url_address}', [BuildingController::class, 'destroy'])->middleware(['auth','verified'])->name('building.destroy');
